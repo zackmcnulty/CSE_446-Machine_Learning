c499c9696c00fecebd1040c0510b5af9cfdede93
Zachary McNulty <zjmcnulty@gmail.com>
2019-05-02 00:57:42 -0700

    finished logistic hw2

--- hw/hw2/logistic.py
+++ hw/hw2/logistic.py
@@ -4,16 +4,16 @@ logistic.py
 '''
 
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 from mnist.loader import MNIST
 
-def grad_J(X, y, w, b,n, lam):
+def grad_J(X, y, w, b, lam):
     '''
     returns a vector that represents the gradient of J at the given points w,b
     '''
-    
-    #nabla_w = 1/n * sum([ -y[i]*X[i, :] * (1/(1+np.exp(-y[i]*(b + X[i, :] * w)) - 1)) for i in range(n) ]) + 2 * lam * w
-    #nabla_b = 1/n * sum([-y[i] * (1/(1+np.exp(-y[i]*(b + X[i, :] * w)) - 1)) for i in range(n)])
+
+    n = y.size
 
     # y is a (n, ) dim vector instead of a (n, 1) vector. We adjust the dims so the elementwise product
     # performed below behaves as expected
@@ -24,9 +24,8 @@ def grad_J(X, y, w, b,n, lam):
     # temp.T as current temp is a row vector
     mu = 1 / (1 + np.exp(np.multiply(-1*y, temp.T)))
 
-    # NOTE: Why do I need these extra minuses at the front?
-    nabla_w = -1/n * np.dot(np.multiply(y, X).T, 1-mu) + 2 * lam * w
-    nabla_b = -1/n * np.dot(y.T, 1-mu)
+    nabla_w = 1/n * np.dot(np.multiply(-y, X).T, 1-mu) + 2 * lam * w
+    nabla_b = 1/n * np.dot(-y.T, 1-mu)
 
     return np.vstack((nabla_w, nabla_b))
 
@@ -38,7 +37,6 @@ def J_function(X, y, w,b, lam):
 
     inside = np.multiply(-y, b + np.dot(w.T, X.T))
     n = y.size
-    print("n: ", n)
 
     # squeeze removes the unnecessary dimensions: i.e. (25,1,1) --> (25,)
     return np.squeeze(1/n * np.sum(np.log(1 + np.exp(inside))) + lam * np.dot(w.T, w))
@@ -84,31 +82,60 @@ def gradient_descent(x_init, gradient_function, eta=0.1, delta=10e-4):
     # x is the best variable values; all_xs shows x value at each iteration
     return (x, all_xs)
 
-def SGD(X, y, x_init, gradient_function, batch_size, eta=0.1, delta=10e-4):
+
+def SGD(X, y, x_init, gradient_function, batch_size, eta=0.1, iteration_num=100):
     '''
     Runs STOCHASTIC gradient descent to calculate optimum
     x_init is the initial values to set to the vector being descended on (in this problem w and b)
     gradient_function is a function that takes in a vector x and outputs its gradient
     eta is the learning rate
-    delta is the stopping condition; stop if all entries in gradient less than delta.
+    iteration_num is the stopping condition; stop after a total of this many batches have been processed.
     '''
     x = x_init
+    n = y.size
     all_xs = [x]
-    grad = gradient_function(x)
-    while np.max(np.abs(grad)) > delta:
-        # perform a step in gradient descent
 
-        print(np.max(np.abs(grad)))
-        x = x - eta * grad
-        grad = gradient_function(x)
-        all_xs.append(x)
+    itr = 0
+    while itr < iteration_num:
+        # Loop through full dataset once, performing a round of updates
+
+        train_order = list(range(n))
+        random.shuffle(train_order)
+
+        for batch_num in range(n // batch_size):
+
+            itr += 1
+            sample = train_order[batch_num * batch_size : (batch_num + 1) * batch_size]
+            X_batch = X[sample, :]
+            y_batch = y[sample]
+
+            grad = gradient_function(X_batch=X_batch, y_batch=y_batch, x=x)
+            x = x - eta * grad
+
+            all_xs.append(x)
+            if itr > iteration_num: break
+
+
+        # handle case where there is one uneven batch left
+        else:
+            if n % batch_size != 0:
+                itr += 1
+                sample = train_order[(n // batch_size) * batch_size : ]
+                X_batch = X[sample ,:]
+                y_batch = y[sample]
+
+                grad = gradient_function(X_batch, y_batch, x)
+                x = x - eta * grad
+
+                all_xs.append(x)
 
     return (x, all_xs)
 
 
 
+# =====================================================================================================================================
 
-# Load MNIST Data
+# Load MNIST Data and filter for 2's and 7's
 mndata = MNIST('./data')
 X_train, labels_train = map(np.array, mndata.load_training())
 X_test, labels_test = map(np.array, mndata.load_testing())
@@ -126,14 +153,15 @@ Y_train = np.where(Y_train == 7, 1, -1)
 Y_test = labels_test[(labels_test == 2) + (labels_test == 7)]
 Y_test = np.where(Y_test == 7, 1, -1)
 
-
+# =====================================================================================================================================
+'''
 n = X_train.shape[0]
 d = X_train.shape[1] # 28 x 28 = 784
 lam = 0.1
 
 # define initial vector and the gradient function
 x_init = np.zeros((d+1, 1)) # = [w, b]
-gradient_function_train = lambda x: grad_J(X_train, Y_train, x[:-1], x[-1], n, lam)
+gradient_function_train = lambda x: grad_J(X_train, Y_train, x[:-1], x[-1], lam)
 delta = 0.01
 eta = 0.1 # learning rate
 
@@ -154,25 +182,102 @@ plt.legend(['Training', 'Testing'])
 # Problem 10b part ii)
 
 plt.figure(2)
-plt.plot([error_rate(X=X_train, y=Y_train, w=x[:-1], b=x[-1]) for x in all_xs_train])
-plt.plot([error_rate(X=X_test, y=Y_test, w=x[:-1], b=x[-1]) for x in all_xs_train]) # use parameters learned from training dataset
+# skips plotting the error rate at iteration zero with w = 0
+plt.plot([error_rate(X=X_train, y=Y_train, w=x[:-1], b=x[-1]) for x in all_xs_train[1:]])
+plt.plot([error_rate(X=X_test, y=Y_test, w=x[:-1], b=x[-1]) for x in all_xs_train[1:]]) # use parameters learned from training dataset
 plt.title('Error rate at each iteration')
 plt.ylabel('Error rate')
-plt.ylim([0,1])
+plt.xlabel('Iteration Number')
+plt.legend(['Training', 'Testing'])
+plt.show()
+
+'''
+
+
+# =====================================================================================================================================
+
+# Problem 10c)
+
+n = X_train.shape[0]
+d = X_train.shape[1] # 28 x 28 = 784
+x_init = np.zeros((d+1, 1)) # = [w, b]
+batch_size = 1
+eta = 0.01 # learning rate 0.00005, delta = 0.01
+lam = 0.1
+
+num_iterations = 200
+
+# This time, the gradient function accepts some data as an input (i.e. the batch)
+sgd_grad_function = lambda X_batch, y_batch, x: grad_J(X_batch, y_batch, x[:-1], x[-1], lam)
+(x_best_sgd, all_xs_sgd) = SGD(X=X_train, y=Y_train, x_init=x_init, gradient_function=sgd_grad_function, \
+                               batch_size=batch_size, eta=eta, iteration_num=num_iterations)
+
+# 10ci)
+
+plt.figure(3)
+plt.plot([J_function(X=X_train, y=Y_train, w=x[:-1], b=x[-1], lam=lam) for x in all_xs_sgd])
+plt.plot([J_function(X=X_test, y=Y_test, w=x[:-1], b=x[-1], lam=lam) for x in all_xs_sgd]) # use parameters learned from training dataset
+plt.title('SGD Function Value at each iteration (batch_size = 1)')
+plt.ylabel('J(w,b)')
+plt.xlabel('Iteration Number')
+plt.legend(['Training', 'Testing'])
+#plt.show()
+
+
+# 10cii)
+
+plt.figure(4)
+# skips plotting the error rate at iteration zero with w = 0
+plt.plot([error_rate(X=X_train, y=Y_train, w=x[:-1], b=x[-1]) for x in all_xs_sgd[1:]])
+plt.plot([error_rate(X=X_test, y=Y_test, w=x[:-1], b=x[-1]) for x in all_xs_sgd[1:]]) # use parameters learned from training dataset
+plt.title('SGD Error rate at each iteration (batch_size = 1)')
+plt.ylabel('Error rate')
 plt.xlabel('Iteration Number')
 plt.legend(['Training', 'Testing'])
 plt.show()
 
 
 
-# Problem 10c)
-
-batch_size = 1
-sgd_grad_function = lambda X_batch, y_batch, x: grad_J()
-(x_best_train, all_xs_train) = SGD(X=X_train, y=Y_train, )
-
-
+# =====================================================================================================================================
 
 
 # Problem 10d)
+n = X_train.shape[0]
+d = X_train.shape[1] # 28 x 28 = 784
+x_init = np.zeros((d+1, 1)) # = [w, b]
 batch_size = 100
+eta = 0.1 # learning rate 0.00005, delta = 0.01
+lam = 0.1
+
+num_iterations = 150
+
+
+# This time, the gradient function accepts some data as an input (i.e. the batch)
+sgd_grad_function = lambda X_batch, y_batch, x: grad_J(X_batch, y_batch, x[:-1], x[-1], lam)
+(x_best_sgd, all_xs_sgd100) = SGD(X=X_train, y=Y_train, x_init=x_init, gradient_function=sgd_grad_function, \
+                                  batch_size=batch_size, eta=eta, iteration_num=num_iterations)
+
+# 10di)
+
+plt.figure(5)
+plt.plot([J_function(X=X_train, y=Y_train, w=x[:-1], b=x[-1], lam=lam) for x in all_xs_sgd100])
+plt.plot([J_function(X=X_test, y=Y_test, w=x[:-1], b=x[-1], lam=lam) for x in all_xs_sgd100]) # use parameters learned from training dataset
+plt.title('SGD Function Value at each iteration (batch_size = 100)')
+plt.ylabel('J(w,b)')
+plt.xlabel('Iteration Number')
+plt.legend(['Training', 'Testing'])
+#plt.show()
+
+
+# 10dii)
+
+plt.figure(6)
+# skips plotting the error rate at iteration zero with w = 0
+plt.plot([error_rate(X=X_train, y=Y_train, w=x[:-1], b=x[-1]) for x in all_xs_sgd100[1:]])
+plt.plot([error_rate(X=X_test, y=Y_test, w=x[:-1], b=x[-1]) for x in all_xs_sgd100[1:]]) # use parameters learned from training dataset
+plt.title('SGD Error rate at each iteration (batch_size = 100)')
+plt.ylabel('Error rate')
+plt.xlabel('Iteration Number')
+plt.legend(['Training', 'Testing'])
+plt.show()
+
